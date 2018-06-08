072cc488343f747b9bfe9fd743b76e21a3cb1476
cwtbt <lw03232@gmail.com>
2018-06-07 20:01:22 -0500

    Initial commit.

---
+++ src/main/App.java
@@ -0,0 +1,16 @@
+package main;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+	}
+	
+}
